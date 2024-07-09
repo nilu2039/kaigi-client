7c12024148ad7aeefbe0160fb0109f68448c591d
Nilanjan Mandal <nilanjanmandal15@gmail.com>
2024-07-09 12:08:52 +0530

    myPeer changes

--- src/hooks/usePeer.ts
+++ src/hooks/usePeer.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { Peer } from "peerjs";
+import { Peer, PeerOptions } from "peerjs";
 import { useEffect, useRef, useState } from "react";
 
 const usePeer = () => {
@@ -10,14 +10,19 @@ const usePeer = () => {
   useEffect(() => {
     if (isPeerSet.current) return;
     isPeerSet.current = true;
+    const peerOptions = {
+      host: process.env.NEXT_PUBLIC_PEER_HOST!,
+      path: "/peer",
+      secure: process.env.NEXT_PUBLIC_NODE_ENV === "production",
+    } as PeerOptions;
     (async () => {
       try {
-        const myPeer = new Peer({
-          host: process.env.NEXT_PUBLIC_PEER_HOST!,
-          path: "/peer",
-          port: 9000,
-          secure: process.env.NEXT_PUBLIC_NODE_ENV === "production",
-        });
+        let myPeer = null;
+        if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
+          myPeer = new Peer(peerOptions);
+        } else {
+          myPeer = new Peer({ ...peerOptions, port: 9000 });
+        }
         setPeer(myPeer);
         myPeer.on("open", (id) => {
           setMyPeerId(id);
