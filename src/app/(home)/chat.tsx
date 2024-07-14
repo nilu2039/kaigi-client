80404a8f2dcbd913204101c69a993d12e5bc752d
Nilanjan Mandal <nilanjanmandal15@gmail.com>
2024-07-14 21:39:17 +0530

    white board drawing.

--- src/app/(home)/chat.tsx
+++ src/app/(home)/chat.tsx
@@ -4,8 +4,10 @@ import { usePlayer } from "@/context/player";
 import { useSocket } from "@/context/socket";
 import { SOCKET_EVENTS } from "@/lib/constants";
 import { cn } from "@/lib/utils";
-import React, { FC, FormEvent, useEffect, useRef, useState } from "react";
-import { Socket } from "socket.io-client";
+import { FC, FormEvent, useEffect, useRef, useState } from "react";
+
+import Whiteboard from "./whiteboard";
+import { Palette } from "lucide-react";
 
 type ChatProps = {
   myPeerId: string | null;
@@ -16,7 +18,7 @@ const Chat: FC<ChatProps> = ({ myPeerId }) => {
   const scrollAreaRef = useRef<HTMLDivElement | null>(null);
   const [chatInput, setChatInput] = useState<string>("");
   const { chats, setChats, roomId, waitingForMatch } = usePlayer();
-
+  const [isDrawingMode, setIsDrawingMode] = useState<boolean>(false);
   const handleChat = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!socket || !myPeerId || !roomId || !chatInput) return;
@@ -41,50 +43,80 @@ const Chat: FC<ChatProps> = ({ myPeerId }) => {
       });
     }
   }, [chats]);
+
+  const toggleDrawingMode = (data: boolean) => {
+    setIsDrawingMode(data);
+  };
+
   return (
     <div className="flex flex-col h-full w-full rounded-lg shadow-xl border-newAccent border-2">
-      <main
-        className="flex-1 overflow-y-auto p-4 space-y-4"
-        ref={scrollAreaRef}
-      >
-        {chats.map((chat, index) => {
-          return (
-            <div
-              className={cn("flex items-end space-x-2", {
-                "justify-end": chat.senderPeerId === myPeerId,
-              })}
-              key={String(index)}
+      {!isDrawingMode ? (
+        <>
+          <main
+            className="flex-1 overflow-y-auto p-4 space-y-4 relative"
+            ref={scrollAreaRef}
+          >
+            <Palette
+              className={`text-newAccent absolute top-4 right-4`}
+              style={{
+                opacity: waitingForMatch ? 0.5 : 1,
+                cursor: waitingForMatch ? "not-allowed" : "pointer",
+              }}
+              onClick={() => {
+                if (waitingForMatch) return;
+                toggleDrawingMode(true);
+              }}
+            />
+            {chats.map((chat, index) => {
+              return (
+                <div
+                  className={cn("flex items-end space-x-2", {
+                    "justify-end": chat.senderPeerId === myPeerId,
+                  })}
+                  key={String(index)}
+                >
+                  <div
+                    className={cn(
+                      "p-2 rounded-lg bg-gray-100 dark:bg-gray-800",
+                      {
+                        "text-white bg-newAccent":
+                          chat.senderPeerId === myPeerId,
+                      }
+                    )}
+                  >
+                    <p className="text-sm">{chat.content}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </main>
+          <div className="w-full flex flex-col items-center">
+            <form
+              className="flex items-center space-x-2 p-2 w-full"
+              onSubmit={handleChat}
             >
-              <div
-                className={cn("p-2 rounded-lg bg-gray-100 dark:bg-gray-800", {
-                  "text-white bg-newAccent": chat.senderPeerId === myPeerId,
-                })}
+              <Input
+                className="flex-1"
+                placeholder="say hi..."
+                value={chatInput}
+                onChange={(e) => {
+                  setChatInput(e.target.value);
+                }}
+                disabled={waitingForMatch}
+              />
+              <Button
+                className="bg-primaryBtn"
+                size="sm"
+                disabled={!roomId || waitingForMatch}
               >
-                <p className="text-sm">{chat.content}</p>
-              </div>
-            </div>
-          );
-        })}
-      </main>
-      <div className="w-full flex flex-col items-center">
-        <form
-          className="flex items-center space-x-2 p-2 w-full"
-          onSubmit={handleChat}
-        >
-          <Input
-            className="flex-1"
-            placeholder="say hi..."
-            value={chatInput}
-            onChange={(e) => {
-              setChatInput(e.target.value);
-            }}
-            disabled={waitingForMatch}
-          />
-          <Button className="bg-primaryBtn" size="sm">
-            Send
-          </Button>
-        </form>
-      </div>
+                Send
+              </Button>
+            </form>
+          </div>
+        </>
+      ) : (
+        <Whiteboard roomId={roomId} toggleDrawingMode={toggleDrawingMode} />
+      )}
     </div>
   );
 };
