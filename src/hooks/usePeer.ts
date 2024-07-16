4f548c872fe5812bd290e318e65832e6fb56f032
Nilanjan Mandal <nilanjanmandal15@gmail.com>
2024-07-17 01:42:59 +0530

    changed turn servers

--- src/hooks/usePeer.ts
+++ src/hooks/usePeer.ts
@@ -23,7 +23,35 @@ const usePeer = () => {
         // } else {
         //   myPeer = new Peer({ ...peerOptions, port: 9000 });
         // }
-        const myPeer = new Peer();
+        const myPeer = new Peer({
+          config: {
+            iceServers: [
+              {
+                urls: "stun:stun.relay.metered.ca:80",
+              },
+              {
+                urls: "turn:global.relay.metered.ca:80",
+                username: "e388bea6b59cd76c012dffb2",
+                credential: "65VXfD0ATAnv07QY",
+              },
+              {
+                urls: "turn:global.relay.metered.ca:80?transport=tcp",
+                username: "e388bea6b59cd76c012dffb2",
+                credential: "65VXfD0ATAnv07QY",
+              },
+              {
+                urls: "turn:global.relay.metered.ca:443",
+                username: "e388bea6b59cd76c012dffb2",
+                credential: "65VXfD0ATAnv07QY",
+              },
+              {
+                urls: "turns:global.relay.metered.ca:443?transport=tcp",
+                username: "e388bea6b59cd76c012dffb2",
+                credential: "65VXfD0ATAnv07QY",
+              },
+            ],
+          },
+        });
         setPeer(myPeer);
         myPeer.on("open", (id) => {
           setMyPeerId(id);
