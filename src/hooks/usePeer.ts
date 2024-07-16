eb7fac819704fc6c77af3a6d3b0182317c9ca917
Nilanjan Mandal <nilanjanmandal15@gmail.com>
2024-07-17 02:51:47 +0530

    using custom turn server

--- src/hooks/usePeer.ts
+++ src/hooks/usePeer.ts
@@ -10,48 +10,29 @@ const usePeer = () => {
   useEffect(() => {
     if (isPeerSet.current) return;
     isPeerSet.current = true;
-    // const peerOptions = {
-    //   host: process.env.NEXT_PUBLIC_PEER_HOST!,
-    //   path: "/peer",
-    //   secure: process.env.NEXT_PUBLIC_NODE_ENV === "production",
-    // } as PeerOptions;
+    const peerOptions = {
+      host: process.env.NEXT_PUBLIC_PEER_HOST!,
+      path: "/peer",
+      secure: process.env.NEXT_PUBLIC_NODE_ENV === "production",
+      config: {
+        iceServers: [
+          { urls: "stun:stun.l.google.com:19302" },
+          {
+            urls: process.env.NEXT_PUBLIC_TURN_URL!,
+            username: process.env.NEXT_PUBLIC_TURN_USERNAME!,
+            credential: process.env.NEXT_PUBLIC_TURN_CREDENTIALS!,
+          },
+        ],
+      },
+    } as PeerOptions;
     (async () => {
       try {
-        // let myPeer = null;
-        // if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
-        //   myPeer = new Peer(peerOptions);
-        // } else {
-        //   myPeer = new Peer({ ...peerOptions, port: 9000 });
-        // }
-        const myPeer = new Peer({
-          config: {
-            iceServers: [
-              {
-                urls: "stun:stun.relay.metered.ca:80",
-              },
-              {
-                urls: "turn:global.relay.metered.ca:80",
-                username: "e388bea6b59cd76c012dffb2",
-                credential: "65VXfD0ATAnv07QY",
-              },
-              {
-                urls: "turn:global.relay.metered.ca:80?transport=tcp",
-                username: "e388bea6b59cd76c012dffb2",
-                credential: "65VXfD0ATAnv07QY",
-              },
-              {
-                urls: "turn:global.relay.metered.ca:443",
-                username: "e388bea6b59cd76c012dffb2",
-                credential: "65VXfD0ATAnv07QY",
-              },
-              {
-                urls: "turns:global.relay.metered.ca:443?transport=tcp",
-                username: "e388bea6b59cd76c012dffb2",
-                credential: "65VXfD0ATAnv07QY",
-              },
-            ],
-          },
-        });
+        let myPeer = null;
+        if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
+          myPeer = new Peer(peerOptions);
+        } else {
+          myPeer = new Peer({ ...peerOptions, port: 9000 });
+        }
         setPeer(myPeer);
         myPeer.on("open", (id) => {
           setMyPeerId(id);
